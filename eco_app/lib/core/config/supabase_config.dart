09c6d4d3eca40e69992261f030093556f3a93af9
Mfemunoz <mfemunoz@poligran.edu.co>
2026-07-29 19:23:28 -0500

    feat(app): connect Flutter with Supabase

---
+++ eco_app/lib/core/config/supabase_config.dart
@@ -0,0 +1,6 @@
+class SupabaseConfig {
+  static const String url = 'https://pwexbwinxpvvfwhcrpbi.supabase.co';
+
+  static const String anonKey =
+      'sb_publishable_F3-C5kTxfD8Vy0FoQTNkTQ_WulJ8wlt';
+}
